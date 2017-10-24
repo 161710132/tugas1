53262b753b8ae160dbff5636c568ab9e15b88f2d
unknown <171610132@smkassalaambandung.sch.id>
2017-10-24 08:31:01 +0700

    tugas pertama saya

---
+++ data.php
@@ -0,0 +1,13 @@
+<?php 
+class Princess{
+
+	//property
+	public $sikap = 'ramah';
+	public $wajah = 'cantik';
+
+}
+
+ $Princess1 = new Princess;
+ echo "Princess itu memiliki sikap yang sangat " .$Princess1-> sikap. '<br>'; 
+ echo "tidak hanya sikap nya saja yang ramah wajah nya pun sangat " .$Princess1-> wajah. '<br>';
+ ?> 
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+//class
+class manusia{
+
+	//property
+	public $berat = 45;
+	public $tinggi = 155;
+
+
+}
+
+   $manusia1 = new manusia;
+   echo "Manusia itu memiliki berat : " .$manusia1-> berat. '<br>';
+   echo "Manusia itu memiliki tinggi : " .$manusia1-> tinggi. '<br>';
+?>
